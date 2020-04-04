ece7914b6dc8f84383694bbbce27e5a7c9e5fc10
Nguyễn Tất Giáp <ntgptit@gmail.com>
2020-04-05 00:25:49 +0700

    Coding SupplierServices

--- src/main/java/com/giapnt/shoppingcart/postgresql/constant/Contants.java
+++ src/main/java/com/giapnt/shoppingcart/postgresql/constant/Contants.java
@@ -3,4 +3,22 @@ package com.giapnt.shoppingcart.postgresql.constant;
 public final class Contants {
    public final static int DUPLICATE_NAME = -1;
    public final static int DATA_NOT_MODIFIED = 0;
+   /**
+    * ACTION_ADD.
+    */
+   private static final int ACTION_ADD = 1;
+   /**
+    * ACTION_DELETE.
+    */
+   private static final int ACTION_DELETE = 2;
+
+   /**
+    * ACTIVE.
+    */
+   private static final int ACTIVE = 1;
+
+   /**
+    * INACTIVE.
+    */
+   private static final int INACTIVE = 0;
 }
--- src/main/java/com/giapnt/shoppingcart/postgresql/services/SupplierServices.java
+++ src/main/java/com/giapnt/shoppingcart/postgresql/services/SupplierServices.java
@@ -1,10 +1,27 @@
 package com.giapnt.shoppingcart.postgresql.services;
 
+import com.giapnt.shoppingcart.postgresql.model.Suppliers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
 public interface SupplierServices {
-	
+
+    List<Suppliers> getAllListSuppliers();
+
+    List<Suppliers> getListSuppliersActive();
+
+    List<Suppliers> getListSuppliersDeactivate();
+
+    Suppliers getSuppliersById(long id);
+
+    Map<Integer, Object> modifySuppliersByPrimaryKey(Suppliers suppliers);
+
+    Map<Integer, Object> addNewSuppliers(Suppliers suppliers);
+
+    Map<Integer, Object> removeSuppliers(Suppliers suppliers);
 }
