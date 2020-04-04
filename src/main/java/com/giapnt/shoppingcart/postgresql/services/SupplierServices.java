9f435436e0c7126f8c4b8fcf65290ec6d2594cdd
Nguyễn Tất Giáp <ntgptit@gmail.com>
2020-04-04 19:22:01 +0700

    Coding SupplierServices

--- src/main/java/com/giapnt/shoppingcart/postgresql/services/SupplierServices.java
+++ src/main/java/com/giapnt/shoppingcart/postgresql/services/SupplierServices.java
@@ -1,9 +1,27 @@
 package com.giapnt.shoppingcart.postgresql.services;
 
+import com.giapnt.shoppingcart.postgresql.model.Category;
+import com.giapnt.shoppingcart.postgresql.model.Suppliers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
 public interface SupplierServices {
+
+    List<Suppliers> findAllSupplier();
+
+    Suppliers findBySupplierId();
+
+    Map<Integer, Object> activeSupplier(Suppliers suppliers);
+
+    Map<Integer, Object> deactiveSupplier(Suppliers suppliers);
+
+    Map<Integer, Object> addNewSupplier(Suppliers suppliers);
+
+    Map<Integer, Object> removeSupplier(Suppliers suppliers);
+
 }
